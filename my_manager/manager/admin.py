8abcf2186ac83984d587f2d0f253fe1c12e8c437
vadbash <vadusikvadim@gmail.com>
2024-04-09 23:00:06 +0300

    admin panel

--- my_manager/manager/admin.py
+++ my_manager/manager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Passwords, CryptoWallets
 
-# Register your models here.
+class PasswordsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'login', 'password', 'secret')
+
+class CryptoWalletsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'secret_phrase', 'key', 'password')
+
+admin.site.register(Passwords, PasswordsAdmin)
+admin.site.register(CryptoWallets, CryptoWalletsAdmin)
