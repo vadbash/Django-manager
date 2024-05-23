ec6c49a5aee52bdd7c71d87a5680e92dbe3d6f3b
vadbash <vadusikvadim@gmail.com>
2024-05-23 21:41:05 +0300

    add job opportunities

--- my_manager/manager/admin.py
+++ my_manager/manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Passwords, CryptoWallets
+from .models import Passwords, CryptoWallets, JobOpportunities
 
 class PasswordsAdmin(admin.ModelAdmin):
     list_display = ('name', 'login', 'password', 'secret')
@@ -7,5 +7,9 @@ class PasswordsAdmin(admin.ModelAdmin):
 class CryptoWalletsAdmin(admin.ModelAdmin):
     list_display = ('name', 'secret_phrase', 'key', 'password')
 
+class JobOpportunitiesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'field_of_activity', 'salary', 'country')
+
 admin.site.register(Passwords, PasswordsAdmin)
 admin.site.register(CryptoWallets, CryptoWalletsAdmin)
+admin.site.register(JobOpportunities, JobOpportunitiesAdmin)
--- my_manager/manager/models.py
+++ my_manager/manager/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-def title_(self, obj):
-    from django.utils.html import format_html
-    return format_html(
-        f'<div style="width: 50px; word-wrap: break-word">{obj.title}</div>'
-    )
-
 class Passwords(models.Model):
     name = models.CharField('Name of app', max_length = 500)
     login = models.CharField('Login', max_length = 500)
@@ -16,6 +10,7 @@ class Passwords(models.Model):
         verbose_name = "Password"
         verbose_name_plural = "Passwords"
 
+
 class CryptoWallets(models.Model):
     name = models.CharField('Wallet name', max_length = 500)
     secret_phrase = models.CharField('Secret phrase', max_length = 500, blank=True)
@@ -25,3 +20,14 @@ class CryptoWallets(models.Model):
     class Meta:
         verbose_name = "Crypto Wallet"
         verbose_name_plural = "Crypto Wallets"
+
+class JobOpportunities(models.Model):
+    name = models.CharField('Name of company', max_length=500)
+    field_of_activity = models.CharField('Field of activity', max_length=500, blank=True)
+    salary = models.CharField('Salary', max_length=500, blank=True)
+    country = models.CharField('Country of office', max_length=500, blank=True)
+
+    class Meta:
+        verbose_name = "Job Opportunity"
+        verbose_name_plural = "Job Opportunities"
+    
