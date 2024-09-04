c54d0d092fcc7c3e9faad1ab164f48f8ede34314
vadbash <vadusikvadim@gmail.com>
2024-09-04 23:03:09 +0300

    create a programming language selection

--- my_manager/manager/admin.py
+++ my_manager/manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Passwords, CryptoWallets, JobOpportunities, MyProjects
+# from .models import YourModel, Language
+from django import forms
 
 
 class PasswordsAdmin(admin.ModelAdmin):
@@ -14,6 +16,28 @@ class JobOpportunitiesAdmin(admin.ModelAdmin):
 class MyProjectsAdmin(admin.ModelAdmin):
     list_display = ('name', 'languages', 'time', 'description', 'kind')
 
+# Created a programming language selection
+
+# class YourModelForm(forms.ModelForm):
+#     class Meta:
+#         model = YourModel
+#         fields = ['primary_language', 'additional_languages']
+
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['primary_language'].widget.attrs.update({'style': 'width: 50%; display: inline-block;'})
+#         self.fields['additional_languages'].widget.attrs.update({'style': 'width: 50%; display: inline-block;'})
+
+# class YourModelAdmin(admin.ModelAdmin):
+#     form = YourModelForm
+#     fieldsets = (
+#         (None, {
+#             'fields': (('primary_language', 'additional_languages'),)
+#         }),
+#     )
+
+# admin.site.register(YourModel, YourModelAdmin)
+# admin.site.register(Language)
 
 admin.site.register(Passwords, PasswordsAdmin)
 admin.site.register(CryptoWallets, CryptoWalletsAdmin)
